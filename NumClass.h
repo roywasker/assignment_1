8b61b4005f8d620f4c8697d54af587ff8816b2a6
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 <royx520@gmail.com>
2022-11-10 20:59:50 +0200

    add return to the comments

--- NumClass.h
+++ NumClass.h
@@ -1,17 +1,26 @@
 
 /* will return if a number is Armstrong number
 An Armstrong number is an n-digit number that is equal to the sum of the nth powers of its digits.
-
+return 1 if its true
+       0 if its false
 */
 int isArmstrong(int);
 
-/* will return if a number is a palindrome */
+/* will return if a number is a palindrome 
+return 1 if its true
+       0 if its false
+*/
 int isPalindrome(int);
 
-/* will return if a number is prime*/
+/* will return if a number is prime
+return 1 if its true
+       0 if its false
+*/
 int isPrime(int);
 
 /*Strong number is a special number whose sum of the factorial of digits is equal to the original number. For Example: 145 is strong number
+return 1 if its true
+       0 if its false
 */
 int isStrong(int);
 
